37158f6966644dcf265879a9960bf928c610bc01
Andrey <andrey198318@gmail.com>
2015-04-03 19:30:07 -0500

    FriendsContract

--- app/src/main/java/com/tarapus/android/friends/FriendsContract.java
+++ app/src/main/java/com/tarapus/android/friends/FriendsContract.java
@@ -1,7 +1,54 @@
 package com.tarapus.android.friends;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by andreytarasenko on 3/30/15.
  */
 public class FriendsContract {
+    interface FriendsColumns {
+        String FRIENDS_NAME = "friends_name";
+        String FRIENDS_PHONE = "friends_phone";
+        String FRIENDS_EMAIL = "friends_email";
+    }
+
+    public static final String CONTENT_AUTHORITY = "com.tarapus.android.friends.provider";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final String PATH_FRIENDS = "friends";
+
+    public static final String[] TOP_LEVEL_PATHS = {
+            PATH_FRIENDS
+    };
+
+    public static class Friends implements FriendsColumns, BaseColumns {
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_FRIENDS).build();
+
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + ".friends";
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + CONTENT_AUTHORITY + ".friend";
+
+        public static Uri buildFriendUri(String friendId) {
+            return BASE_CONTENT_URI.buildUpon().appendEncodedPath(friendId).build();
+        }
+
+        public static String getFriendId(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/tarapus/android/friends/FriendsDatabase.java
+++ app/src/main/java/com/tarapus/android/friends/FriendsDatabase.java
@@ -11,16 +11,13 @@ public class FriendsDatabase extends SQLiteOpenHelper {
     private static final String TAG = FriendsDatabase.class.getSimpleName();
     private static final String DATABASE_NAME = "friends.db";
     private static final int DATABASE_VERSION = 2; //Needs to be 2 for Upgrades
-
     private final Context mContext; //Store context
 
     interface Tables {
-
         String FRIENDS = "friends";
     }
 
     public FriendsDatabase(Context context){
-
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         mContext = context;
 
