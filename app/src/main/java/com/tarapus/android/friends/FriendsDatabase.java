7fb10a4e3cc76e9de9b024eef4c497e715476d2f
Andrey <andrey198318@gmail.com>
2015-04-04 09:38:46 -0500

    Friends Database code

--- app/src/main/java/com/tarapus/android/friends/FriendsDatabase.java
+++ app/src/main/java/com/tarapus/android/friends/FriendsDatabase.java
@@ -1,7 +1,9 @@
 package com.tarapus.android.friends;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by andreytarasenko on 3/30/15.
@@ -17,11 +19,36 @@ public class FriendsDatabase extends SQLiteOpenHelper {
         String FRIENDS = "friends";
     }
 
-    public FriendsDatabase(Context context){
+    public FriendsDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         mContext = context;
-
     }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("CREATE TABLE " + Tables.FRIENDS + " ("
+                + BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + FriendsContract.FriendsColumns.FRIENDS_NAME + " TEXT NOT NULL,"
+                + FriendsContract.FriendsColumns.FRIENDS_PHONE + " TEXT NOT NULL,"
+                + FriendsContract.FriendsColumns.FRIENDS_EMAIL + " TEXT NOT NULL,");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        int version = oldVersion;
+        if (version == 1) {
+            //Add some extra fields to the database without deleting any existing data
+            version = 2;
+        }
+        if (version != DATABASE_VERSION) {
+            db.execSQL("DROP TABLE IF EXISTS " + Tables.FRIENDS);
+            onCreate(db);
+        }
+    }
+
+    public static void deleteDatabase(Context context) {
+        context.deleteDatabase(DATABASE_NAME);
+    }
 }
