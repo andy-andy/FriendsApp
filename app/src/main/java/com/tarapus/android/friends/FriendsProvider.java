8b8e2c414de2a06d2ee75ff67e03e908f42e680b
Andrey <andrey198318@gmail.com>
2015-04-04 15:50:36 -0500

    Addition to the FriendsProvider class

--- app/src/main/java/com/tarapus/android/friends/FriendsProvider.java
+++ app/src/main/java/com/tarapus/android/friends/FriendsProvider.java
@@ -1,7 +1,100 @@
 package com.tarapus.android.friends;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.util.Log;
+
 /**
  * Created by andreytarasenko on 3/30/15.
  */
-public class FriendsProvider {
+public class FriendsProvider extends ContentProvider {
+
+    private FriendsDatabase mOpenHelper;
+    private static String TAG = FriendsProvider.class.getSimpleName();
+    private static final UriMatcher sUriMatcher = buildUriMatcher;
+
+
+    private static final int FRIENDS = 100;
+    private static final int FRIENDS_ID = 101;
+
+    private static UriMatcher buildUriMatcher() {
+
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = FriendsContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, "friends", FRIENDS);
+        matcher.addURI(authority, "friends/*", FRIENDS_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new FriendsDatabase(getContext());
+        return true;
+    }
+
+    private void deleteDatabase() {
+        mOpenHelper.close();
+        FriendsDatabase.deleteDatabase(getContext());
+        mOpenHelper = new FriendsDatabase(getContext());
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case FRIENDS:
+                return FriendsContract.Friends.CONTENT_TYPE;
+            case FRIENDS_ID:
+                return FriendsContract.Friends.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(FriendsDatabase.Tables.FRIENDS);
+
+        switch(match){
+            case FRIENDS:
+                // return nothing(Requesting all data)
+                break;
+            case FRIENDS_ID:
+                String id = FriendsContract.Friends.getFriendId(uri);
+                queryBuilder.appendWhere(BaseColumns._ID + "=" + id);
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+        Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        Log.v(TAG, "insert(uri= " + uri + ", values= " + values.toString());
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch(match){
+            case FRIENDS:
+                long recordId = db.insertOrThrow(FriendsDatabase.Tables.FRIENDS, null, values);
+                return FriendsContract.Friends.buildFriendUri(recordId);
+                default:
+                    throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+    }
+
+
 }
+
