7636bbc3d472ded2122d462815afe3f459aadf4a
Andrey <andrey198318@gmail.com>
2015-04-06 21:33:03 -0500

    Friends custom adapter

---
+++ app/src/main/java/com/tarapus/android/friends/FriendsCustomAdapter.java
@@ -0,0 +1,55 @@
+package com.tarapus.android.friends;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by andreytarasenko on 4/6/15.
+ */
+public class FriendsCustomAdapter extends ArrayAdapter<Friend> {
+    private LayoutInflater mLayoutInflater;
+    private static FragmentManager sFragmentManager;
+
+    public FriendsCustomAdapter(Context context, FragmentManager fragmentManager ){
+        super(context, android.R.layout.simple_list_item_2);
+        mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        sFragmentManager = fragmentManager;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final View view;
+        if(convertView == null){
+            view = mLayoutInflater.inflate(R.layout.custom_friend, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        final Friend friend = getItem(position);
+        final int _id = friend.getId();
+        final String name = friend.getName();
+        final String phone = friend.getPhone();
+        final String email = friend.getEmail();
+
+        ((TextView) view.findViewById(R.id.friend_name)).setText(name);
+        ((TextView) view.findViewById(R.id.friend_phone)).setText(phone);
+        ((TextView) view.findViewById(R.id.friend_email)).setText(email);
+
+        Button editButton = (Button) view.findViewById(R.id.edit);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), EditActivity.class);
+
+            }
+        });
+
+    }
+}
