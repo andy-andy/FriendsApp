e8e94efc251ac0432e9c6287e56573712c896462
Andrey <andrey198318@gmail.com>
2015-04-07 23:51:07 -0500

    Friends Custom Adapter done!

--- app/src/main/java/com/tarapus/android/friends/FriendsCustomAdapter.java
+++ app/src/main/java/com/tarapus/android/friends/FriendsCustomAdapter.java
@@ -2,6 +2,7 @@ package com.tarapus.android.friends;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 /**
  * Created by andreytarasenko on 4/6/15.
  */
@@ -17,7 +20,7 @@ public class FriendsCustomAdapter extends ArrayAdapter<Friend> {
     private LayoutInflater mLayoutInflater;
     private static FragmentManager sFragmentManager;
 
-    public FriendsCustomAdapter(Context context, FragmentManager fragmentManager ){
+    public FriendsCustomAdapter(Context context, FragmentManager fragmentManager) {
         super(context, android.R.layout.simple_list_item_2);
         mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         sFragmentManager = fragmentManager;
@@ -26,7 +29,7 @@ public class FriendsCustomAdapter extends ArrayAdapter<Friend> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final View view;
-        if(convertView == null){
+        if (convertView == null) {
             view = mLayoutInflater.inflate(R.layout.custom_friend, parent, false);
         } else {
             view = convertView;
@@ -47,9 +50,37 @@ public class FriendsCustomAdapter extends ArrayAdapter<Friend> {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getContext(), EditActivity.class);
+                intent.putExtra(FriendsContract.FriendsColumns.FRIENDS_ID, String.valueOf(_id));
+                intent.putExtra(FriendsContract.FriendsColumns.FRIENDS_NAME, name);
+                intent.putExtra(FriendsContract.FriendsColumns.FRIENDS_PHONE, phone);
+                intent.putExtra(FriendsContract.FriendsColumns.FRIENDS_EMAIL, email);
 
+                getContext().startActivity(intent);
             }
         });
 
+        Button deleteButton = (Button) view.findViewById(R.id.delete);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FriendsDialog dialog = new FriendsDialog;
+                Bundle args = new Bundle();
+                args.putString(FriendsDialog.DIALOG_TYPE, FriendsDialog.DELETE_RECORD);
+                args.putString(FriendsContract.FriendsColumns.FRIENDS_ID, String.valueOf(_id));
+                args.putString(FriendsContract.FriendsColumns.FRIENDS_NAME, name);
+                dialog.setArguments(args);
+                dialog.show(sFragmentManager, "delete-record")
+            }
+        });
+        return view;
+    }
+
+    public void setData(List<Friend> friends) {
+        clear();
+        if (friends != null) {
+            for (Friend friend : friends) {
+                add(friend);
+            }
+        }
     }
 }
