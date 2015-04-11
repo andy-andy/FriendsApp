b00316a7303a0ed4a98ac4e55a364ad3b325393f
Andrey <andrey198318@gmail.com>
2015-04-10 20:52:08 -0500

    Addition to the Main activity/FriendsDialog

--- app/src/main/java/com/tarapus/android/friends/FriendsDialog.java
+++ app/src/main/java/com/tarapus/android/friends/FriendsDialog.java
@@ -17,6 +17,7 @@ public class FriendsDialog extends DialogFragment {
     private LayoutInflater mLayoutInflater;
     public static final String DIALOG_TYPE = "command";
     public static final String DELETE_RECORD = "deleteRecord";
+    public static final String DELETE_DATABASE = "deleteDatabase";
 
     @NonNull
     @Override
@@ -29,12 +30,26 @@ public class FriendsDialog extends DialogFragment {
             final int _id = getArguments().getInt(FriendsContract.FriendsColumns.FRIENDS_ID);
             String name = getArguments().getString(FriendsContract.FriendsColumns.FRIENDS_NAME);
             TextView popupMessage = (TextView) view.findViewById(R.id.popup_message);
-            popupMessage.setText("Are you sure you want to delete" + name + "from the database");
+            popupMessage.setText("Are you sure you want to delete" + name + "from the database?");
             builder.setView(view).setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     ContentResolver contentResolver = getActivity().getContentResolver();
                     Uri uri = FriendsContract.Friends.buildFriendUri(String.valueOf(_id));
+                    contentResolver.delete(uri, null, null);
+                    Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            });
+        } else if (command.equals(DELETE_DATABASE)) {
+            TextView popupMessage = (TextView) view.findViewById(R.id.popup_message);
+            popupMessage.setText("Are you sure you wish to delete the entire Database?");
+            builder.setView(view).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ContentResolver contentResolver = getActivity().getContentResolver();
+                    Uri uri = FriendsContract.URI_TABLE;
                     contentResolver.delete(uri, null, null);
                     Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
